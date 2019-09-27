64aa2d33b3dfd7c4772996ec69601e74ca021cb0
Amin <ayman201266@hotmail.com>
2019-09-27 10:29:28 +0300

    The Web service back-end done !

--- server/api/controllers/DrugsInfoController.js
+++ server/api/controllers/DrugsInfoController.js
@@ -1,15 +1,18 @@
 const getDrugsInfo = require('../../database/queries/getDrugsInfo');
 
-const DrugsController = (req, res) => {
-  const { drugCode, diseaseCode, type } = req.params;
 
-  getDrugsInfo(drugCode, diseaseCode, type)
-  .then((drugs) => {
-    if (!drugs || drugs.length === 0)
-    {
-      return res.status(401).json({ message: 'no data exists' });
+const DrugsController = (req, res) => {
+  const { drugCode, diseaseCode, type } = req.query;
+
+  getDrugsInfo(drugCode, diseaseCode, type, (err, drugs) => {
+    if (err) {
+      res.status(501).json({ message: 'There is an server Error' });
     }else {
-      return res.json({ drugs });
+      if (!drugs || drugs.length === 0){
+        res.status(401).json({ message: 'No drugs exists' });
+      } else {
+        res.json(drugs.rows);
+      }
     }
   });
 }
