d45284c3610461b41bbdc1024b7782c159fb46e9
Amin <ayman201266@hotmail.com>
2019-10-02 02:13:40 +0300

    Locally DB tag #2

--- server/database/dbConnection.js
+++ server/database/dbConnection.js
@@ -2,11 +2,10 @@ const {Pool} = require('pg');
 require("env2")("config.env")
 
 if (process.env.DBNum == 1) {
-  const DATABASE_URL = process.env.DATABASE_URL;
+  var DATABASE_URL = process.env.DATABASE_URL;
 } else {
-  const DATABASE_URL = process.env.DB_URL;
+  var DATABASE_URL = process.env.DB_URL;
 }
-
 
 if (!DATABASE_URL) {
 throw Error ("No database URL");
