367631b34564234ea79e75b408a4ecfe773a5d44
Amin <ayman201266@hotmail.com>
2019-10-01 17:16:09 +0300

    optmize deloying

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.use(router);
 
 
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build','index.html'));
 });
