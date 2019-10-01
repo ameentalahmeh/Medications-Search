5ab0b802a3aea0e9567c7648b484c70ab8625b54
Amin <ayman201266@hotmail.com>
2019-10-01 13:14:41 +0300

    trying 3

---
+++ client/build/precache-manifest.1ee51db7917592b0095f508dac49ea25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8862b2895b548a59808532c3211178b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7d394c363fa89378ad7",
+    "url": "/static/css/main.cad79024.chunk.css"
+  },
+  {
+    "revision": "98b5f50c745f2a6197cf",
+    "url": "/static/js/2.823583fc.chunk.js"
+  },
+  {
+    "revision": "b7d394c363fa89378ad7",
+    "url": "/static/js/main.d8a63cc4.chunk.js"
+  },
+  {
+    "revision": "5178fb100f9f6a9821ca",
+    "url": "/static/js/runtime-main.2877809b.js"
+  }
+]);
