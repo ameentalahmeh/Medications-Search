66ddf70a19e05dc246568b8b40fbdf9bf1cf14d7
Amin <ayman201266@hotmail.com>
2019-10-02 02:37:37 +0300

    optmize Deploying stage #1

--- server/database/dbConnection.js
+++ server/database/dbConnection.js
@@ -1,11 +1,8 @@
 const {Pool} = require('pg');
 require("env2")("config.env")
 
-if (process.env.DBNum == 1) {
-  var DATABASE_URL = process.env.DATABASE_URL;
-} else {
-  var DATABASE_URL = process.env.DB_URL;
-}
+const {DATABASE_URL} = process.env;
+
 
 if (!DATABASE_URL) {
 throw Error ("No database URL");
