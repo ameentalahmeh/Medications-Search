3f84e3dcf2c1a087482093535c0d4ac16b06b10b
Amin <ayman201266@hotmail.com>
2019-10-02 08:53:54 +0300

    handling errors

--- server/api/controllers/ErrorsController.js
+++ server/api/controllers/ErrorsController.js
@@ -1,12 +1,12 @@
 // -- Error 404 -------------------------
 const errorNotFound = (req, res) => {
-    res.writeHead(404, {'Content-Type' : 'text/html'});
+    // res.writeHead(404, {'Content-Type' : 'text/html'});
     res.end('<h1> Page Not Found </h1>');
 };
 
 // -- Error 505 -------------------------
 const serverError = (req, res) => {
-    res.writeHead(505, {'Content-Type' : 'text/html'});
+    // res.writeHead(505, {'Content-Type' : 'text/html'});
     res.end('<h1> There is an server Error </h1>');
 };
 
