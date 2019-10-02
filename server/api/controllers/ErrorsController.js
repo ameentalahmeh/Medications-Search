56d570414126b0db346fbf7dc8d8b70febafe52c
Amin <ayman201266@hotmail.com>
2019-10-02 09:10:22 +0300

    updating err handling

--- server/api/controllers/ErrorsController.js
+++ server/api/controllers/ErrorsController.js
@@ -1,13 +1,11 @@
 // -- Error 404 -------------------------
 const errorNotFound = (req, res) => {
-    // res.writeHead(404, {'Content-Type' : 'text/html'});
-    res.end('<h1> Page Not Found </h1>');
+    res.send('<h1>Sorry, we cannot find that!</h1>')
 };
 
 // -- Error 505 -------------------------
 const serverError = (req, res) => {
-    // res.writeHead(505, {'Content-Type' : 'text/html'});
-    res.end('<h1> There is an server Error </h1>');
+    res.send('<h1> There is an server Error </h1>');
 };
 
 module.exports = {errorNotFound, serverError}
