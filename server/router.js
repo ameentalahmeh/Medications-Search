76b037043f9fe170f2dde6eecbfabadc8d4e0477
Amin <ayman201266@hotmail.com>
2019-10-02 10:16:42 +0300

    updating err handling adding middelware

--- server/router.js
+++ server/router.js
@@ -11,6 +11,7 @@ const router = express.Router();
 
 router.get('/api/getMedicationsInfo', auth, MedicationsController);
 
+router.use(auth)
 router.use(ErrorsController.errorNotFound);
 router.use(ErrorsController.serverError);
 module.exports = router;
