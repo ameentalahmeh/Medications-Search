b854922479ae18e28ebd87a132865a9666bcaa6d
Amin <ayman201266@hotmail.com>
2019-10-02 05:00:12 +0300

    solving type field issue

--- server/api/controllers/MedicationsInfoController.js
+++ server/api/controllers/MedicationsInfoController.js
@@ -4,16 +4,22 @@ const  ErrorsController  = require('./ErrorsController.js');
 
 const MedicationsController = (req, res) => {
   const { drugCode, diseaseCode, type } = req.query;
-  getMedicationsInfo(drugCode, diseaseCode, type, (err, medications) => {
-    if (err) {
+  console.log(req.query);
+  if (type > 2 || type < 1 ) {
+      ErrorsController.serverError(req,res);
+  }
+  else {
+    getMedicationsInfo(drugCode, diseaseCode, type, (err, medications) => {
+      if (err) {
         ErrorsController.serverError(req,res);
-    }else {
-      if (medications.rows && medications.rows.length !== 0){
-        res.json({code: 1, success: true, message: 'Success!', medications:medications.rows});
-      } else {
-        res.json({code: 2, success: true ,message: 'No Medications exists !!'});
+      }else {
+        if (medications.rows && medications.rows.length !== 0){
+          res.json({code: 1, success: true, message: 'Success!', medications:medications.rows});
+        } else {
+          res.json({code: 2, success: true ,message: 'No Medications exists !!'});
+        }
       }
-    }
-  });
+    });
+  }
 }
 module.exports = MedicationsController;
--- server/database/models/MedicationModel.sql
+++ server/database/models/MedicationModel.sql
@@ -7,15 +7,27 @@ CREATE TABLE drug (
   decription      TEXT          NOT NULL,
   drugCode        VARCHAR(300)  NOT NULL,
   diseaseCode     VARCHAR(300)  NOT NULL,
-  type            INTEGER       NOT NULL  CHECK (type = 1 OR type = 2)
+  type            INTEGER    NOT NULL  CHECK (type = 1 OR type = 2)
 );
 
 INSERT INTO drug (decription,drugCode,diseaseCode,type) VALUES
 ('Acetaminophen','xaw123','aaa444',1),
-('Citalopram','xaw123','aaa444',1),
-('Trofin','xaw123','aaa444',1),
+('Citalopram','xaw123','aaa444',2),
+('Trofin','xaw123','aaa444',2),
 ('Acamol','xaw123','aaa444',1),
 ('Zenat','xaw123','aaa444',1),
+('Lexapro','asp123','ccc666',2),
+('Omeprazole','asp456','ddd777',2),
+('Ativan','rfn123','eee888',1),
+('Xanax','rfn456','fff999',2);
+('Lexapro','asp123','ccc666',2),
+('Omeprazole','asp456','ddd777',2),
+('Ativan','rfn123','eee888',1),
+('Xanax','rfn456','fff999',2);
+('Lexapro','asp123','ccc666',2),
+('Omeprazole','asp456','ddd777',2),
+('Ativan','rfn123','eee888',1),
+('Xanax','rfn456','fff999',2);
 ('Lexapro','asp123','ccc666',2),
 ('Omeprazole','asp456','ddd777',2),
 ('Ativan','rfn123','eee888',1),
