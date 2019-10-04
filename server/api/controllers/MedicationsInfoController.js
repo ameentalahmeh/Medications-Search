5a652ad84aa1670765a45368c618f4e8c04efec6
Amin <ayman201266@hotmail.com>
2019-10-05 02:46:16 +0300

    solve some issues stage1

--- server/api/controllers/MedicationsInfoController.js
+++ server/api/controllers/MedicationsInfoController.js
@@ -18,7 +18,9 @@ const MedicationsController = (req, res) => {
      const LegalURLflag = Object.keys(InputsAsObject).map((i,index) => i === legalURLKeys[index]);
      const {drugCode, diseaseCode, type} = InputsAsObject;
 
-     if (type > 2 || type < 1 || Object.keys(InputsAsObject).length !== legalURLKeys.length || LegalURLflag.includes(false) ){
+
+
+     if (parseInt(type) > 2 || parseInt(type) < 1 ){
        ErrorsController.serverError(req,res);
      }
      else {
