0db85daf576ef008d5c9dd018c5732fc700e07e6
Amin <ayman201266@hotmail.com>
2019-10-05 07:00:22 +0300

    optmize MDS

---
+++ client/build/precache-manifest.54e61f179bb075927a6ad7925e2a7460.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799deaa8885448a7e4314ad1fbd2423e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f89e81eadf5d1830939",
+    "url": "/static/css/main.847e9153.chunk.css"
+  },
+  {
+    "revision": "12631620cfb91db2c7fd",
+    "url": "/static/js/2.68cf4342.chunk.js"
+  },
+  {
+    "revision": "0f89e81eadf5d1830939",
+    "url": "/static/js/main.30919c6d.chunk.js"
+  },
+  {
+    "revision": "5178fb100f9f6a9821ca",
+    "url": "/static/js/runtime-main.2877809b.js"
+  }
+]);
