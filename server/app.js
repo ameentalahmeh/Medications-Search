7707a85d3d3c17ab414cf2346252d20fb443f9de
Amin <ayman201266@hotmail.com>
2019-10-05 19:52:59 +0300

    acceleration MDS 7

--- server/app.js
+++ server/app.js
@@ -4,13 +4,12 @@ const router = require('./router');
 
 const app = express();
 
-app.use('/', express.static(path.join(__dirname, '..', 'client', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
 app.use(router);
-
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'client', 'build','index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build','index.html'));
+});
 
 app.set('port', process.env.PORT || 3002);
 module.exports = app;
