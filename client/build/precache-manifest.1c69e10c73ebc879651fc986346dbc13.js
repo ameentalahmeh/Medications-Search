e804b08492511fff4a164a42c81d92a5100c0fac
Amin <ayman201266@hotmail.com>
2019-10-05 19:32:11 +0300

    acceleration MDS 3

---
+++ client/build/precache-manifest.1c69e10c73ebc879651fc986346dbc13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4556a981c01c01cee84ec763db88eeac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "023c8f8ebdc3d890c80e",
+    "url": "/static/css/main.847e9153.chunk.css"
+  },
+  {
+    "revision": "12631620cfb91db2c7fd",
+    "url": "/static/js/2.68cf4342.chunk.js"
+  },
+  {
+    "revision": "023c8f8ebdc3d890c80e",
+    "url": "/static/js/main.82bb7402.chunk.js"
+  },
+  {
+    "revision": "5178fb100f9f6a9821ca",
+    "url": "/static/js/runtime-main.2877809b.js"
+  }
+]);
--- server/app.js
+++ server/app.js
@@ -4,12 +4,12 @@ const router = require('./router');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.use(router);
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'public','index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'build','index.html'));
 });
 
 app.set('port', process.env.PORT || 3002);
